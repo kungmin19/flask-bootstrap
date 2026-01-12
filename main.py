42b98bd55c78114c7ac5e3864df1752d06a26e6f
kungmin19 <natthawutkingsanthia@gmail.com>
2026-01-12 10:48:10 +0700

    Edit Two

--- main.py
+++ main.py
@@ -1,52 +1,87 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'nakdnawjdnawndawndawnda'
+app.config['SECRET_KEY'] = b'hgkjfytdfguilikoiu'
 
 cars = [
-   {'id': 1, 'brand': 'Toyota', 'model': 'Yaris Ativ', 'year': 2024, 'price': 59000},
-   {'id': 2, 'brand': 'Honda', 'model': 'City Hatchback', 'year': 2024, 'price': 65003},
-   {'id': 3, 'brand': 'Nissan', 'model': 'Amera', 'year': 2025, 'price': 59000},
+  {'id':1, 'brand': 'Toyota', 'model':'Yaris Ativ', 'year': 2024, 'price': 570000},
+  {'id':2, 'brand':'Honda', 'model':'City Hatchback', 'year': 2024, 'price': 650000},
+  {'id':3, 'brand': 'Nissan', 'model':'Amera', 'year': 2025, 'price': 550000}
 ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home Page')
+  return render_template('index.html', title='Home Page')
 
-@app.route('/cars')
+@app.route('/cars', methods=['GET', 'POST'])
 def all_cars():
-    return render_template('cars/cars.html', title='Show All Cars Page', cars=cars)
+  if request.method == 'POST':
+    brand = request.form['brand']
+    tmp_cars = []
+    for car in cars:
+      if brand in car['brand']:
+        tmp_cars.append(car)
+    # cars = tmp_cars
+    return render_template('cars/cars.html', 
+                         title='Search Cars Page',
+                         cars=tmp_cars)
+  
+  return render_template('cars/cars.html', 
+                         title='Show All Cars Page',
+                         cars=cars)
 
 @app.route('/cars/new', methods=['GET', 'POST'])
 def new_car():
-    if request.method == 'POST':
-        brand = request.form['brand']
-        model = request.form['model']
-        year = int(request.form['year'])
-        price = int(request.form['price'])
+  if request.method == 'POST':
+    brand = request.form['brand']
+    model = request.form['model']
+    year = int(request.form['year'])
+    price = int(request.form['price'])
 
-        length = len(cars)
-        id = cars[length-1]['id']+1
+    length = len(cars)
+    id = cars[length-1]['id'] + 1
 
-        car = {'id': id, 'brand': brand, 'model': model, 'year': year, 'price': price}
+    car = {'id':id, 'brand': brand, 'model':model, 'year': year, 'price': price}
 
-        cars.append(car)
-        flash('Add new Car Successfully!', 'success')
-        return redirect(url_for('all_cars'))
-    
-    return render_template('cars/new_car.html', title='New Car Page')
+    cars.append(car)
+    flash('Add new Car Successfull.', 'success')
+    return redirect(url_for('all_cars'))
 
+  return render_template('cars/new_car.html',
+                          title='New Car Page')
 
 @app.route('/cars/<int:id>/delete')
 def delete_car(id):
-    for car in cars:
-        if id == car['id']:
-            cars.remove(car)
-            flash('Delete Car Successfully!', 'success')
-            break
-    return redirect(url_for('all_cars'))
+  for car in cars:
+    if id == car['id']:
+      cars.remove(car)
+      break
+  flash('Delete car successfull', 'success')
+  return redirect(url_for('all_cars'))
 
-
-@app.route('/cars/<int:id>/delete')
+@app.route('/cars/<int:id>/Edit', methods=['GET', 'POST'])
 def Edit_car(id):
-   return render_template('cars/edit_car.html', title='Edit Car Page')
+  for c in cars:
+    if id == c['id']:
+      car = c
+      break
+  if request.method == 'POST':
+    brand = request.form['brand']
+    model = request.form['model']
+    year = int(request.form['year'])
+    price = int(request.form['price'])
+
+    for c in cars:
+      if id == c['id']:
+        c['brand'] = brand
+        c['model'] = model
+        c['year'] = year
+        c['price'] = price
+        break
+    
+    flash('Update Car Successfull.', 'success')
+    return redirect(url_for('all_cars'))
+  
+  return render_template('cars/edit_car.html',
+                         title='Edit Car Page',
+                         car=car)
